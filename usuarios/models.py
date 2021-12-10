f6d4fa815fb992e3da303f8ed13e02519c571fe2
reginaldo <reginaldo.costa@gruporedeamazonica.com.br>
2021-12-10 16:52:10 -0400

    criando aplicação usuario persoanlizado

---
+++ usuarios/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+# Create your models here.
+
+class UsuarioManager(BaseUserManager):
+    
+    def create_user(self, email, password=None):
+        usuario = self.model(
+            email=self.normalize_email(email),
+        )
+
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+
+        if password:
+            usuario.set_password(password)
+
+        usuario.save()
+
+        return usuario
+
+    def create_superuser(self, email, password):
+        usuario = self.create_user(
+            email = self.normalize_email(email),
+            password=password,
+        )
+
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+
+        usuario.set_password(password)
+        usuario.save()
+
+        return usuario
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="E-mail do usuário", max_length=254, unique=True)
+    
+    is_active = models.BooleanField(verbose_name="Usuário ativo?", default=True)
+    
+    is_staff = models.BooleanField(verbose_name="Usuário é da equipe de desenvolvimento?", default=False)
+    
+    is_superuser = models.BooleanField(verbose_name="Usuário é um superusuário?", default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UsuarioManager()
+
+    class Meta:
+        db_table = 'usuario'
+        managed = True
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários '
+
+    def __str__(self):
+        return self.email
