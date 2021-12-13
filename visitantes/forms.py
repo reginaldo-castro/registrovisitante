0f6de96dee1a1b38c9800ae77c1f013b493555c3
reginaldo <reginaldo.costa@gruporedeamazonica.com.br>
2021-12-13 11:23:56 -0400

    personaloizando os dados a serem mostrados e configurando as mensagens de campos obrigatorios

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -4,4 +4,22 @@ from visitantes.models import Visitante
 class visitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = [
+            "nome_completo", "cpf", "data_nascimento",
+            "setor_visitado", "placa_veiculo",
+        ]
+        error_messages = {
+                "nome_completo": {
+                    "required": "O nome completo do visitante é obrigatório para registro"
+                },
+                "cpf": {
+                    "required": "O CPF do visitante é obrigatório para o registro"
+                },
+                "data_nascimento": {
+                    "required": "A data de nascimento do visitante é obrigatório",
+                    "invalid": "Por favor, informe um formato válido para a data de nascimento"
+                },
+                "setor_visitado": {
+                    "required": "Por favor, informe o setor a ser visitado"
+                }
+            }
