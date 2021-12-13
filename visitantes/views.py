2c5c381e1e018c307fb46f33aead9c2732e5b19b
reginaldo <reginaldo.costa@gruporedeamazonica.com.br>
2021-12-13 11:20:59 -0400

    criando o adicionar e detalhes de um visitante

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import redirect, render, get_object_or_404
 from visitantes.forms import visitanteForm
+from visitantes.models import Visitante
 
 def registrar_visitante(request):
-
     form = visitanteForm
+
+    if request.method == "POST":
+        form = visitanteForm(request.POST)
+       
+        if form.is_valid():
+            visitante = form.save(commit=False)
+
+            visitante.registrado_por = request.user.porteiro
+        
+            visitante.save()
+
+            messages.success(
+                request,
+                "Visitante registrado com sucesso"
+            )
+            return redirect("index")
 
     context = {
         "nome_pagina":  "Registrar visitante",
@@ -12,3 +28,16 @@ def registrar_visitante(request):
     }
 
     return render(request, 'registrar_visitante.html', context)
+
+def informacoes_visistantes(request, id):
+
+    visitante = get_object_or_404(Visitante, id=id)
+
+
+    context = {
+        "nome_pagina": "Informações do visitante",
+        "visitante": visitante
+    }
+    return render(request, 'informacoes_visitante.html', context)
+
+
